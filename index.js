e807dcdc4b1fc3ae83dd6d867b1a461cb573b575
folafunmi-db <folafunmimustapha@gmail.com>
2020-10-05 17:37:59 +0100

    Added code to start http server at part 3000

---
+++ index.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 10 */
+
+// Primary file for the API
+
+
+// Dependencies
+const http = require("http");
+
+// Server should respond to all requests with a string
+let server = http.createServer(function (req, res) {
+  res.end("Hello World\n");
+});
+
+// Start the server and have it listen on port 3000
+server.listen(3000, function () {
+  console.log("The server is listening");
+});
