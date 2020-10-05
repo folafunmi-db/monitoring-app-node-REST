352cea8d620d7f1ea4bae501a12aa75e9e865d51
folafunmi-db <folafunmimustapha@gmail.com>
2020-10-05 18:02:48 +0100

    Added code to get path without any trailing slash

--- index.js
+++ index.js
@@ -2,13 +2,26 @@
 
 // Primary file for the API
 
-
 // Dependencies
 const http = require("http");
+const url = require("url");
 
 // Server should respond to all requests with a string
 let server = http.createServer(function (req, res) {
-  res.end("Hello World\n");
+  // Get the path
+  let parsedURL = url.parse(req.url, true);
+
+  // Get path
+  let path = parsedURL.pathname;
+
+  // Trim off any extraneous slashes with regex
+  let trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  // Send the response
+  res.end("Done. Check your response.\n");
+
+  // Log the requested path
+  console.log(`Request received on path: ${trimmedPath}`);
 });
 
 // Start the server and have it listen on port 3000
