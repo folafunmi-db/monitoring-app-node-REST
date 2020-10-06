5befb4e37912ea50d22901d8035809defc1a596f
folafunmi-db <folafunmimustapha@gmail.com>
2020-10-06 16:26:49 +0100

    Added code to obtain payloads sent with request

--- index.js
+++ index.js
@@ -5,6 +5,7 @@
 // Dependencies
 const http = require("http");
 const url = require("url");
+const StringDecoder = require("string_decoder").StringDecoder;
 
 // Server should respond to all requests with a string
 let server = http.createServer(function (req, res) {
@@ -26,19 +27,40 @@ let server = http.createServer(function (req, res) {
   //  Get the headers as an object
   let headers = req.headers;
 
-  // Send the response
-  res.end("Done. Check your response.\n");
+  // Get payloads, if any
+  let decoder = new StringDecoder("utf-8");
 
-  // Log the requested path
-  console.log(
-    `\n Request received on path: ${trimmedPath} \n
-  With method: ${method} \n
-  Query parameters: `,
-    queryStringObject
-  );
+  // To cater for streams
+  let buffer = "";
 
-  // Log received headers
-  console.log("\n Request received with these headers: ", headers);
+  // Add to the buffer with each new stream of data object that's released
+  // by the request
+  req.on("data", function (data) {
+    buffer += decoder.write(data);
+  });
+
+  // On end of the request
+  req.on("end", function () {
+    buffer += decoder.end();
+
+    // Send the response
+    res.end("Done. Check your response.\n");
+
+    // Log the requested path
+    console.log(
+      `\n
+    Request received on path: ${trimmedPath} \n
+    With method: ${method} \n
+    Query parameters: `,
+      queryStringObject
+    );
+
+    // Log received headers
+    console.log("\n Request received with these headers: ", headers);
+
+    // Log received payload
+    console.log("\n Request received with these payloads: ", buffer);
+  });
 });
 
 // Start the server and have it listen on port 3000
